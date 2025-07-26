e4d0fec0bb341a8e94d2b43a971318df8889e3b2
Nikita Baniya <nikitabaniya57@gmail.com>
2025-07-26 17:13:56 +0545

    updated about

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,6 +1,11 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGithub } from "react-icons/fa";
-import { SiTailwindcss, SiTypescript, SiVite, SiAppwrite } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiTypescript,
+  SiVite,
+  SiAppwrite,
+} from "react-icons/si";
 
 export const About = () => {
   const skills = [
@@ -15,10 +20,12 @@ export const About = () => {
     { name: "Appwrite", icon: <SiAppwrite className="text-pink-500" /> },
   ];
   return (
-    <section id="about" className="min-h-screen flex flex-col items-center justify-center py-20">
+    <section
+      id="about"
+      className="min-h-screen flex flex-col items-center justify-center py-20"
+    >
       <RevealOnScroll>
         <div className="max-w-5xl mx-auto px-4 w-full space-y-16">
-          
           {/* About Me Title */}
           <h2 className="text-4xl font-bold bg-gradient-to-r from-[#e196a1] to-[#8f2d49] bg-clip-text text-transparent text-center">
             About Me
@@ -26,12 +33,15 @@ export const About = () => {
 
           {/* Education Section */}
           <div>
-            <h3 className="text-2xl font-semibold text-center text-[#4e342e] mb-6">Education</h3>
+            <h3 className="text-2xl font-semibold text-center text-[#4e342e] mb-6">
+              Education
+            </h3>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-
               {/* Secondary */}
               <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:shadow-md transition bg-[#FFDEDE]">
-                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">Secondary</h4>
+                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">
+                  Secondary
+                </h4>
                 <ul className="list-disc list-inside text-[#4e342e] ml-2 space-y-2">
                   <li>Shree Rastriya Rotary Secondary School (2015)</li>
                 </ul>
@@ -49,7 +59,9 @@ export const About = () => {
 
               {/* Higher Secondary */}
               <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:shadow-md transition bg-[#FFDEDE]">
-                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">Higher Secondary (NEB)</h4>
+                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">
+                  Higher Secondary (NEB)
+                </h4>
                 <ul className="list-disc list-inside text-[#4e342e] ml-2 space-y-2">
                   <li>Hetauda School Of Management (2015-2017)</li>
                 </ul>
@@ -67,7 +79,9 @@ export const About = () => {
 
               {/* Bachelor */}
               <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:shadow-md transition bg-[#FFDEDE]">
-                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">Bachelor in Computer Application (TU)</h4>
+                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">
+                  Bachelor in Computer Application (TU)
+                </h4>
                 <ul className="list-disc list-inside text-[#4e342e] ml-2 space-y-2">
                   <li>Birendra Multiple Campus (2018–2022)</li>
                 </ul>
@@ -82,21 +96,29 @@ export const About = () => {
                   </a>
                 </div> */}
               </div>
-
             </div>
           </div>
 
           {/* Internship Experience Section */}
           <div>
-            <h3 className="text-2xl font-semibold text-center text-[#4e342e] mb-6">Internship Experience</h3>
+            <h3 className="text-2xl font-semibold text-center text-[#4e342e] mb-6">
+              Internship Experience
+            </h3>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              
               {/* Internship 1 */}
               <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:shadow-md transition bg-[#FFDEDE]">
-                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">PHP Developer - InnovateX (2021)</h4>
+                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">
+                  PHP Developer - InnovateX (2021)
+                </h4>
                 <ul className="list-disc list-inside text-[#4e342e] ml-2 space-y-2">
-                  <li>Developed and maintained backend features using core PHP and MySQL.</li>
-                  <li>Worked on open source software projects, contributing to code improvement, customization, and feature enhancement.</li>
+                  <li>
+                    Developed and maintained backend features using core PHP and
+                    MySQL.
+                  </li>
+                  <li>
+                    Worked on open source software projects, contributing to
+                    code improvement, customization, and feature enhancement.
+                  </li>
                 </ul>
                 {/* <div className="mt-4">
                   <a
@@ -112,12 +134,26 @@ export const About = () => {
 
               {/* Internship 2 */}
               <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:shadow-md transition bg-[#FFDEDE]">
-                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">React Developer – Icon Soft (2024–Present)</h4>
+                <h4 className="text-xl font-bold mb-2 text-[#4e342e]">
+                  React Developer – Icon Soft (2025)
+                </h4>
                 <ul className="list-disc list-inside text-[#4e342e] ml-2 space-y-2">
-                  <li>Developing and maintaining scalable, responsive web applications using React.js and JavaScript (ES6+).</li>
-                  <li>Building reusable components and frontend libraries for future use.</li>
-                  <li>Implementing state management using Redux, ReduxTookit or Context API as per project requirements.</li>
-                  <li>Writing clean, maintainable, and well-documented code following best practices.</li>
+                  <li>
+                    Developing and maintaining scalable, responsive web
+                    applications using React.js and JavaScript (ES6+).
+                  </li>
+                  <li>
+                    Building reusable components and frontend libraries for
+                    future use.
+                  </li>
+                  <li>
+                    Implementing state management using Redux, ReduxTookit or
+                    Context API as per project requirements.
+                  </li>
+                  <li>
+                    Writing clean, maintainable, and well-documented code
+                    following best practices.
+                  </li>
                 </ul>
                 {/* <div className="mt-4">
                   <a
@@ -135,24 +171,26 @@ export const About = () => {
 
           {/* Tech Skills Section */}
           <div>
-            <h3 className="text-2xl font-semibold text-center text-[#4e342e] mb-6">Technical Skills</h3>
+            <h3 className="text-2xl font-semibold text-center text-[#4e342e] mb-6">
+              Technical Skills
+            </h3>
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:shadow-md transition bg-[#FFDEDE]">
               <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 max-w-4xl mx-auto">
-               {skills.map((skill, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center text-center transition-transform duration-200 transform hover:scale-110"
-            whileHover={{ scale: 1.1 }}
-          >
-            <div className="text-5xl mb-2">{skill.icon}</div>
-            <p className="text-gray-700 font-medium">{skill.name}</p>
+                {skills.map((skill, index) => (
+                  <div
+                    key={index}
+                    className="flex flex-col items-center text-center transition-transform duration-200 transform hover:scale-110"
+                    whileHover={{ scale: 1.1 }}
+                  >
+                    <div className="text-5xl mb-2">{skill.icon}</div>
+                    <p className="text-gray-700 font-medium">{skill.name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-        ))}
-      </div>
-      </div>
-    </div>
-  </div>
-  </RevealOnScroll>
+        </div>
+      </RevealOnScroll>
     </section>
   );
 };
